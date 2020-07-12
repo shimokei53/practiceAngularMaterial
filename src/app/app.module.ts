0a76bee76a81f31226b95a2ae62e2d0089e3856b
shimokei53 <respect.assistance.may.313@gmail.com>
2020-07-12 17:35:55 +0900

    ng generate component components/practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PracticeComponent } from './components/practice/practice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PracticeComponent
   ],
   imports: [
     BrowserModule,
